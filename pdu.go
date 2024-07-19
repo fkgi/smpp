84692e8edef596e7f312c743e610e8b612ff35b1
fkgi <ats.fukagai@gmail.com>
2024-07-19 20:39:23 +0900

    update

--- pdu.go
+++ pdu.go
@@ -124,13 +124,14 @@ type DataSM struct {
 	DstAddr  string `json:"dst_addr"`
 	EsmClass byte   `json:"esm_class"`
 
-	RegisteredDelivery byte              `json:"registered_delivery"`
-	DataCoding         byte              `json:"data_coding"`
-	Param              map[uint16][]byte `json:"options,omitempty"`
+	RegisteredDelivery byte `json:"registered_delivery"`
+	DataCoding         byte `json:"data_coding"`
+
+	Param map[uint16][]byte `json:"options,omitempty"`
 }
 
 func (*DataSM) CommandID() CommandID {
-	return 0x00000103
+	return DataSm
 }
 
 func (d *DataSM) Marshal() []byte {
@@ -184,13 +185,14 @@ func (d *DataSM) Unmarshal(data []byte) (e error) {
 }
 
 type DataSM_resp struct {
-	Status    uint32            `json:"status"`
-	MessageID string            `json:"id"`
-	Param     map[uint16][]byte `json:"options,omitempty"`
+	Status    uint32 `json:"status"`
+	MessageID string `json:"id"`
+
+	Param map[uint16][]byte `json:"options,omitempty"`
 }
 
 func (*DataSM_resp) CommandID() CommandID {
-	return 0x80000103
+	return DataSmResp
 }
 
 func (d *DataSM_resp) Marshal() []byte {
@@ -229,131 +231,7 @@ func (d *DataSM_resp) CommandStatus() uint32 {
 	return d.Status
 }
 
-type SubmitSM struct {
-	SvcType  string
-	SrcTON   byte
-	SrcNPI   byte
-	SrcAddr  string
-	DstTON   byte
-	DstNPI   byte
-	DstAddr  string
-	EsmClass byte
-
-	ProtocolId           byte
-	PriorityFlag         byte
-	ScheduleDeliveryTime string
-	ValidityPeriod       string
-	RegisteredDelivery   byte
-	ReplaceIfPresentFlag byte
-	DataCoding           byte
-	SmDefaultMsgId       byte
-	// SmLength            byte
-	ShortMessage []byte
-
-	Param map[uint16][]byte
-}
-
-func (*SubmitSM) CommandID() CommandID {
-	return 0x00000006
-}
-
-func (d *SubmitSM) Marshal() []byte {
-	w := bytes.Buffer{}
-
-	writeCString([]byte(d.SvcType), &w)
-	w.WriteByte(d.SrcTON)
-	w.WriteByte(d.SrcNPI)
-	writeCString([]byte(d.SrcAddr), &w)
-	w.WriteByte(d.DstTON)
-	w.WriteByte(d.DstNPI)
-	writeCString([]byte(d.DstAddr), &w)
-	w.WriteByte(d.EsmClass)
-	w.WriteByte(d.ProtocolId)
-	w.WriteByte(d.PriorityFlag)
-	writeCString([]byte(d.ScheduleDeliveryTime), &w)
-	writeCString([]byte(d.ValidityPeriod), &w)
-	w.WriteByte(d.RegisteredDelivery)
-	w.WriteByte(d.ReplaceIfPresentFlag)
-	w.WriteByte(d.DataCoding)
-	w.WriteByte(d.SmDefaultMsgId)
-	// w.WriteByte(d.SmLength)
-	w.WriteByte(byte(len(d.ShortMessage)))
-	w.Write(d.ShortMessage)
-
-	for k, v := range d.Param {
-		writeTLV(k, v, &w)
-	}
-
-	return w.Bytes()
-}
-
-func (d *SubmitSM) Unmarshal(data []byte) (e error) {
-	buf := bytes.NewBuffer(data)
-	var l byte
-	if d.SvcType, e = readCString(buf); e != nil {
-	} else if d.SrcTON, e = buf.ReadByte(); e != nil {
-	} else if d.SrcNPI, e = buf.ReadByte(); e != nil {
-	} else if d.SrcAddr, e = readCString(buf); e != nil {
-	} else if d.DstTON, e = buf.ReadByte(); e != nil {
-	} else if d.DstNPI, e = buf.ReadByte(); e != nil {
-	} else if d.DstAddr, e = readCString(buf); e != nil {
-	} else if d.EsmClass, e = buf.ReadByte(); e != nil {
-	} else if d.ProtocolId, e = buf.ReadByte(); e != nil {
-	} else if d.PriorityFlag, e = buf.ReadByte(); e != nil {
-	} else if d.ScheduleDeliveryTime, e = readCString(buf); e != nil {
-	} else if d.ValidityPeriod, e = readCString(buf); e != nil {
-	} else if d.RegisteredDelivery, e = buf.ReadByte(); e != nil {
-	} else if d.ReplaceIfPresentFlag, e = buf.ReadByte(); e != nil {
-	} else if d.DataCoding, e = buf.ReadByte(); e != nil {
-	} else if d.SmDefaultMsgId, e = buf.ReadByte(); e != nil {
-	} else if l, e = buf.ReadByte(); e != nil {
-	} else {
-		d.ShortMessage = make([]byte, int(l))
-		_, e = buf.Read(d.ShortMessage)
-	}
-	if e == nil {
-		d.Param = make(map[uint16][]byte)
-		for {
-			t, v, e2 := readTLV(buf)
-			if e2 == io.EOF {
-				break
-			}
-			if e2 != nil {
-				e = e2
-				break
-			}
-			d.Param[t] = v
-		}
-	}
-	return
-}
-
-type SubmitSM_resp struct {
-	Status    uint32 `json:"status"`
-	MessageID string `json:"id"`
-}
-
-func (*SubmitSM_resp) CommandID() CommandID {
-	return 0x80000006
-}
-
-func (d *SubmitSM_resp) Marshal() []byte {
-	w := bytes.Buffer{}
-	writeCString([]byte(d.MessageID), &w)
-	return w.Bytes()
-}
-
-func (d *SubmitSM_resp) Unmarshal(data []byte) (e error) {
-	buf := bytes.NewBuffer(data)
-	d.MessageID, e = readCString(buf)
-	return
-}
-
-func (d *SubmitSM_resp) CommandStatus() uint32 {
-	return d.Status
-}
-
-type DeliverSM struct {
+type smPDU struct {
 	SvcType  string `json:"svc_type"`
 	SrcTON   byte   `json:"src_ton,omitempty"`
 	SrcNPI   byte   `json:"src_npi,omitempty"`
@@ -377,11 +255,7 @@ type DeliverSM struct {
 	Param map[uint16][]byte `json:"options,omitempty"`
 }
 
-func (*DeliverSM) CommandID() CommandID {
-	return 0x00000005
-}
-
-func (d *DeliverSM) Marshal() []byte {
+func (d *smPDU) Marshal() []byte {
 	w := bytes.Buffer{}
 
 	writeCString([]byte(d.SvcType), &w)
@@ -411,7 +285,7 @@ func (d *DeliverSM) Marshal() []byte {
 	return w.Bytes()
 }
 
-func (d *DeliverSM) Unmarshal(data []byte) (e error) {
+func (d *smPDU) Unmarshal(data []byte) (e error) {
 	buf := bytes.NewBuffer(data)
 	var l byte
 	if d.SvcType, e = readCString(buf); e != nil {
@@ -452,13 +326,70 @@ func (d *DeliverSM) Unmarshal(data []byte) (e error) {
 	return
 }
 
+type SubmitSM struct {
+	smPDU
+}
+
+func (*SubmitSM) CommandID() CommandID {
+	return SubmitSm
+}
+
+func (d *SubmitSM) Marshal() []byte {
+	return d.smPDU.Marshal()
+}
+
+func (d *SubmitSM) Unmarshal(data []byte) (e error) {
+	return d.smPDU.Unmarshal(data)
+}
+
+type SubmitSM_resp struct {
+	Status    uint32 `json:"status"`
+	MessageID string `json:"id"`
+}
+
+func (*SubmitSM_resp) CommandID() CommandID {
+	return SubmitSmResp
+}
+
+func (d *SubmitSM_resp) Marshal() []byte {
+	w := bytes.Buffer{}
+	writeCString([]byte(d.MessageID), &w)
+	return w.Bytes()
+}
+
+func (d *SubmitSM_resp) Unmarshal(data []byte) (e error) {
+	buf := bytes.NewBuffer(data)
+	d.MessageID, e = readCString(buf)
+	return
+}
+
+func (d *SubmitSM_resp) CommandStatus() uint32 {
+	return d.Status
+}
+
+type DeliverSM struct {
+	smPDU
+}
+
+func (*DeliverSM) CommandID() CommandID {
+	return DeliverSm
+}
+
+func (d *DeliverSM) Marshal() []byte {
+	return d.smPDU.Marshal()
+}
+
+func (d *DeliverSM) Unmarshal(data []byte) (e error) {
+	return d.smPDU.Unmarshal(data)
+}
+
 type DeliverSM_resp struct {
 	Status    uint32 `json:"status"`
 	MessageID string `json:"id,omitempty"`
 }
 
 func (*DeliverSM_resp) CommandID() CommandID {
-	return 0x80000005
+	return DeliverSmResp
 }
 
 func (d *DeliverSM_resp) Marshal() []byte {
